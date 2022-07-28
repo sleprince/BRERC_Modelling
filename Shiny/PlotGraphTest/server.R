d8b87d0a70e91d5d1cf6349ed10041aeef4e9c23
Stavrin <steffan.bculture@gmail.com>
2022-07-28 12:41:50 +0100

    Huge progress, can now click the button and it runs DataDiagnostics and spits out the generated plot and text.

--- Shiny/PlotGraphTest/server.R
+++ Shiny/PlotGraphTest/server.R
@@ -9,16 +9,53 @@
 
 library(shiny)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+# Define server logic required
+shinyServer(function(input, output, clientData) {
   
   foom <- function() {
     
+    output$myImage <- renderImage({
+      # A temp file to save the output.
+      # This file will be removed later by renderImage
+      outfile <- tempfile(fileext='.png')
+      
+      
+      
+      # Generate the PNG
+      png(outfile, width=400, height=320)
+      DoDiagnostics()
+      dev.off() #main="Generated in renderImage()"
+      
+      # Return a list containing the filename
+      list(src = outfile,
+           contentType = 'image/png',
+           width = 400,
+           height = 320,
+           alt = "This is alternate text")
+    }, deleteFile = TRUE)
+    
+    output$textWithHTML <- renderUI({
+      rawText <- readLines('lm_output.txt') # get raw text
+      
+      # split the text into a list of character vectors
+      #   Each element in the list contains one line
+      splitText <- stringi::stri_split(str = rawText, regex = '\\n')
+      
+      # wrap a paragraph tag around each element in the list
+      replacedText <- lapply(splitText, p)
+      
+      return(replacedText)
+    })
     
     # message(modelListSummary$coefficients)
-    message_string <- "Hello there"
-    print(message_string)
-    message(message_string)
+    #message_string <- "Hello there"
+    #print(message_string)
+    
+    #message(message_string)
+    #message()
+    #message("one")
+    #Sys.sleep(0.5)
+    #message("two")
   }
   
   observeEvent(input$btn, {
@@ -33,21 +70,11 @@ shinyServer(function(input, output) {
     })
     
       })
+
 })
 
-    output$distPlot <- renderPlot({
 
-        # generate bins based on input$bins from ui.R
-#        x    <- faithful[, 2]
-#        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        #bins <- 2
-        # draw the histogram with the specified number of bins
-      plot(graph <- dataDiagnostics(taxa = MyData$Species,
-                                    site = MyData$Site,
-                                    time_period = MyData$dateofrecord,
-                                    progress_bar = FALSE,
-                                    plot = TRUE))
-          
-    })
 
-#})
+
+
+
--- Shiny/PlotGraphTest/ui.R
+++ Shiny/PlotGraphTest/ui.R
@@ -14,6 +14,7 @@ library (ggplot2)
 
 source("DataDiagnostics.R")
 
+DoDiagnostics <<- function() {
 setwd("C:/BRERC")
 
 MyData<-read.csv("BRERC2_dataset_vsmall2.csv") #this is the CSV made in run first?
@@ -23,52 +24,67 @@ MyData<-read.csv("BRERC2_dataset_vsmall2.csv") #this is the CSV made in run firs
 MyData$dateofrecord <- as.Date(MyData$dateofrecord)
 class(MyData$dateofrecord)
 
-
 # Run some data diagnostics on our data
 #and save the results to a png file
-png(file="C:/BRERC/saving_plot.png",
-    width=600, height=350)
-#hist(Temperature, col="gold")
+#png(file="C:/BRERC/saving_plot.png",
+#    width=600, height=350)
 results <- dataDiagnostics(taxa = MyData$Species,
                            site = MyData$Site,
                            time_period = MyData$dateofrecord,
                            progress_bar = FALSE)
-dev.off()
+#dev.off()
 
-resultas <- B()
-graph <- dataDiagnostics(taxa = MyData$Species,
-                                  site = MyData$Site,
-                                  time_period = MyData$dateofrecord,
-                                  progress_bar = FALSE,
-                                  plot = TRUE)
+#write results that display in console to a txt file.
+sink(file = "lm_output.txt")
+B()
+sink() #end diversion of output
 
-plot(graph <- dataDiagnostics(taxa = MyData$Species,
-                              site = MyData$Site,
-                              time_period = MyData$dateofrecord,
-                              progress_bar = FALSE,
-                              plot = TRUE))
+}
 
-print(resultas <- B())
+# Run some data diagnostics on our data
+#and save the results to a png file
+#png(file="C:/BRERC/saving_text.png",
+#width=600, height=350)
+#B()
+#dev.off()
+
+
+#resultas <<- B()
+
+#print(resultas <- B())
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("BRERC Data Tool"),
 
-    # Sidebar with a slider input for number of bins
+    # Sidebar 
 #    sidebarLayout(
 #        sidebarPanel(
             shinyjs::useShinyjs(),
             actionButton("btn","Click me"),
             textOutput("text"),
             
-#          )
-#        ),
+sidebarLayout(
+  
+  # Sidebar with a slider input
+  sidebarPanel(
+    
+    uiOutput('textWithHTML')
+    #sliderInput("obs",
+    #            "Number of observations:",
+    #            min = 0,
+    #            max = 1000,
+    #            value = 500)
+  ),
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    imageOutput("myImage")
+  )
+#textOutput('textWithNewlines'),
 
-        # Show a plot of the generated distribution
-#        mainPanel(
-            plotOutput("distPlot")
-#        )
     )
+)
 )
