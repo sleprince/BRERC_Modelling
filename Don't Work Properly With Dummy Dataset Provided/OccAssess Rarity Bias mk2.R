9ce82455a11e23c702450dd230aa892607a71282
Stavrin <steffanleprince@gmail.com>
2022-11-28 19:49:57 +0000

    Further trying to fix OccAssess.

--- Don't Work Properly With Dummy Dataset Provided/OccAssess Rarity Bias mk2.R
+++ Don't Work Properly With Dummy Dataset Provided/OccAssess Rarity Bias mk2.R
@@ -8,16 +8,22 @@ library(occAssess)
 #library(rnrfa)
 #library(sparta)
 
-MyData<-read.csv("ALLBCCv1.csv") #This is the CSV you made in 'run first'
+MyData<-read.csv("ALLBCCv1pt1.csv") #This is the CSV you made in 'run first'
+MyData<-read.csv("tiny.csv")
+
+
 
 str(MyData)
 
 periods <- list(1950:1959, 1960:1969, 1970:1979, 1980:1989, 1990:1999, 2000:2009, 2010:2019)
 
 #clean up data
+MyData2 <- MyData[, c("Species", "east", "north", "Year", "Uncertainty",
+                      "taxagroup")]
 
+rm(MyData)
 
-taxBias <- assessRarityBias(dat = MyData,
+taxBias <- assessRarityBias(dat = MyData2,
                             periods = periods,
                             res = 0.5,
                             prevPerPeriod = FALSE,
