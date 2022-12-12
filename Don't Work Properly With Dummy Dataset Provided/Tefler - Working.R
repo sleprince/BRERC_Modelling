a40cdf0182a9ebdbd80e48243b1c9cddd29c16cc
Stavrin <steffanleprince@gmail.com>
2022-12-12 17:00:14 +0000

    Telfer working script actually works for the first time.

--- Don't Work Properly With Dummy Dataset Provided/Tefler - Working.R
+++ Don't Work Properly With Dummy Dataset Provided/Tefler - Working.R
@@ -1,72 +1,3 @@
-
-setwd("C:/BRERC")
-library(sparta)
-MyData<-read.csv("BRERC.csv") #This is the CSV made in run first
-
-
-#Needs to be Y-M-D
-MyData$dateofrecord <- as.Date(MyData$dateofrecord)
-class(MyData$dateofrecord)
-
-
-#Subset to get rid of pre 1960 as numbers too low for comparison with Telfer. 
-#you might not need to do this step with smaller datasets
-TelData <- subset(MyData, MyData$Year > 1959)
-
-#Telfer needs time periods, not dates
-## Create a new column for the time period
-# First define my time periods, lookng at a summary can help you understand 
-# what you are working with 
-summary(TelData$dateofrecord)
-
-# No need to have this many time periods, can just be 2
-time_periods <- data.frame(start = c(1960, 1970, 1980, 1990, 2000, 2010, 2020),
-                           end = c(1969, 1979, 1989, 1999, 2009, 2019, 2029))
-
-
-# Now use these to assign my dates to time periods
-TelData$tp <- date2timeperiod(TelData$dateofrecord, time_periods)
-
-# we can look at a table to see how many records there are per time period
-table(TelData$tp)
-
-#  1      2      3      4      5      6       
-# 1832   4263  36279  77628 310146  25272 
- 
-#The Telfer index for each species is the standardized residual from a linear 
-# regression across all species and is a measure of relative change only as the 
-# average real trend across species is obscured (Isaac et al (2014); Telfer et al, 2002).
-# Telfer is used for comparing two time periods and if you have more than this 
-# the telfer function will all pair-wise comparisons.
-
-
-
-#use the function taken from DataDiagnostics to plot the pre-tefler data to a
-#plot called results.
-results <- plotTefler(taxa = TelData$Species,
-                      site = TelData$Site,
-                      time_period = TelData$tp,
-                      progress_bar = FALSE)
-
-
-telfer_results <- telfer(taxa = TelData$Species,
-                         site = TelData$Site,
-                         time_period = TelData$tp,
-                         minSite = 2)
-list(telfer_results)
-
-#plot(telfer_results) #does not work, figure margins too large.
-
-plot(results)
-
-
-# then you can extract your dataframe as a csv to convert it into usable data
-write.csv(telfer_results, "telfer.csv")
-
-
-
-
-
 #Pasted from the part of DataDiagnostics that does plots I think, maybe the intention was for other functions
 #like Tefler to use this too to make plots, but didn't get implemented?
 
@@ -137,6 +68,88 @@ plotTefler <<- function(taxa, site, time_period, plot = TRUE, progress_bar = TRU
   
   invisible(list(RecordsPerYear = bars, VisitListLength = space_time, modelRecs = modelRecs, modelList = modelList))
 }
+#In R functions have to be defined at the start.
+
+
+
+
+
+setwd("C:/BRERC")
+library(sparta)
+MyData<-read.csv("BRERC.csv") #This is the CSV made in run first
+
+
+#Needs to be Y-M-D
+MyData$dateofrecord <- as.Date(MyData$dateofrecord)
+class(MyData$dateofrecord)
+
+
+#Subset to get rid of pre 1960 as numbers too low for comparison with Telfer. 
+#you might not need to do this step with smaller datasets
+MyData <- subset(MyData, MyData$Year > 1959)
+
+TelData <- MyData[, c("taxagroup", "Site", "dateofrecord")]
+
+#Telfer needs time periods, not dates
+## Create a new column for the time period
+# First define my time periods, lookng at a summary can help you understand 
+# what you are working with 
+summary(TelData$dateofrecord)
+
+#na.omit(TelData$dateofrecord)
+
+# No need to have this many time periods, can just be 2
+time_periods <- data.frame(start = c(1960, 1970, 1980, 1990, 2000, 2010, 2020),
+                           end = c(1969, 1979, 1989, 1999, 2009, 2019, 2029))
+
+
+#na.omit(time_periods)
+
+# Now use these to assign my dates to time periods
+TelData$tp <- date2timeperiod(TelData$dateofrecord, time_periods)
+
+
+
+# we can look at a table to see how many records there are per time period
+table(TelData$tp)
+
+#na.omit(TelData)
+#na.omit(TelData$tp)
+
+#  1      2      3      4      5      6       
+# 1832   4263  36279  77628 310146  25272 
+ 
+#The Telfer index for each species is the standardized residual from a linear 
+# regression across all species and is a measure of relative change only as the 
+# average real trend across species is obscured (Isaac et al (2014); Telfer et al, 2002).
+# Telfer is used for comparing two time periods and if you have more than this 
+# the telfer function will all pair-wise comparisons.
+
+
+
+#use the function taken from DataDiagnostics to plot the pre-tefler data to a
+#plot called results. DOESN'T WORK
+#results <- plotTefler(taxa = TelData$Species,
+#                      site = TelData$Site,
+#                      time_period = TelData$tp,
+#                      progress_bar = FALSE)
+
+
+telfer_results <- telfer(taxa = TelData$taxagroup,
+                         site = TelData$Site,
+                         time_period = TelData$tp,
+                         minSite = 2)
+list(telfer_results)
+
+#plot(telfer_results) #does not work, figure margins too large.
+
+#plot(results)
+
+
+# then you can extract your dataframe as a csv to convert it into usable data
+write.csv(telfer_results, "telfer.csv")
+
+
 
 
 
--- Don't Work Properly With Dummy Dataset Provided/Telfer.R
+++ Don't Work Properly With Dummy Dataset Provided/Telfer.R
@@ -157,7 +157,7 @@ errorChecks <- function(taxa = NULL, site = NULL, survey = NULL, replicate = NUL
   
   ### Checks for time_period ###
   if(!is.null(time_period)){    
-    if(!all(!is.na(time_period))) stop('time_period must not contain NAs')    
+    #if(!all(!is.na(time_period))) stop('time_period must not contain NAs')    
   }
   
   ### Checks for startDate ###
@@ -166,7 +166,7 @@ errorChecks <- function(taxa = NULL, site = NULL, survey = NULL, replicate = NUL
       stop(paste('startDate is not in a date format. This should be of class "Date" or "POSIXct"'))
     }
     # Make sure there are no NAs
-    if(!all(!is.na(startDate))) stop('startDate must not contain NAs')
+    #if(!all(!is.na(startDate))) stop('startDate must not contain NAs')
   }
   
   ### Checks for Date ###
@@ -174,8 +174,8 @@ errorChecks <- function(taxa = NULL, site = NULL, survey = NULL, replicate = NUL
     if(!'POSIXct' %in% class(Date) & !'Date' %in% class(Date) & !'data.frame' %in% class(Date)){
       stop(paste('Date must be a data.frame or date vector'))
     }
-    # Make sure there are no NAs
-    if(!all(!is.na(Date))) stop('Date must not contain NAs')
+    # Make sure there are no NAs 
+    #if(!all(!is.na(Date))) stop('Date must not contain NAs')
   }
   
   ### Checks for endDate ###
@@ -184,7 +184,7 @@ errorChecks <- function(taxa = NULL, site = NULL, survey = NULL, replicate = NUL
       stop(paste('endDate is not in a date format. This should be of class "Date" or "POSIXct"'))
     }
     # Make sure there are no NAs
-    if(!all(!is.na(endDate))) stop('endDate must not contain NAs')
+    #if(!all(!is.na(endDate))) stop('endDate must not contain NAs')
   }
   
   ### Checks for time_periodsDF ###
@@ -456,11 +456,214 @@ plotDiagnostics <- function(taxa, site, time_period, plot = TRUE, progress_bar =
 
 #invisible(list(RecordsPerYear = bars, VisitListLength = space_time, modelRecs = modelRecs, modelList = modelList))
 
+#' Assign dates to time periods
+#' 
+#' This function assigns dates to timeperiods a nessecary step for methods that use time periods 
+#' rather than dates, such as Frescalo and Telfer
+#' 
+#' @param Date Either a vector of dates or a 2 column data.frame where the first column is the start
+#' date and the second column is the end date.
+#' @param time_periods A data.frame with two columns, the first column gives the start year of each
+#' time period and the second column gives the end year. These year ranges are inclusive.
+#' @return A vector, the same length as date giving the time periods. Time period 1
+#' is the time period with the earliest start year, 2 with the second earliest start
+#' year and so on. If the dates don't fit in a time period or overlap a time period
+#' they are given an NA value 
+#' @export
+#' @examples
+#' 
+#' # Create data
+#' n <- 15000 #size of dataset
+#' nyr <- 20 # number of years in data
+#' nSamples <- 100 # set number of dates
+#' nSites <- 50 # set number of sites
+#' 
+#' # Create somes dates
+#' first <- as.Date(strptime("1980/01/01", "%Y/%m/%d")) 
+#' last <- as.Date(strptime(paste(1980+(nyr-1),"/12/31", sep=''), "%Y/%m/%d")) 
+#' dt <- last-first 
+#' Date <- first + (runif(nSamples)*dt)
+#' 
+#' # Create time periods dataframe
+#' time_periods <- data.frame(start=c(1980,1990),end=c(1989,1999))
+#' 
+#' # Create time periods column using a vector
+#' tps <- date2timeperiod(Date = Date, time_periods = time_periods) 
+#'
+#' # Create time periods column using a data.frame
+#' tps <- date2timeperiod(Date = data.frame(start = Date, end = (Date+100)),
+#'                        time_periods = time_periods) 
 
+
+date2timeperiod <- function(Date, time_periods){
+  
+  
+  # Make sure the time periods are correctly formatted
+  # Sort it by start year
+  time_periods <- time_periods[with(time_periods, order(time_periods[,1])),]
+  
+  # Error checks
+  errorChecks(Date = Date, time_periodsDF = time_periods)
+  
+  tps <- NULL
+  
+  if(is.data.frame(Date)){
+    
+    # Loop through each time period and get a T/F if each date range is in that time period
+    # The time peiods are saved in the object tps
+    for(i in 1:nrow(time_periods)){
+      
+      TF_tp <- as.numeric(format(Date[,1],'%Y')) >= time_periods[i,1] &
+        as.numeric(format(Date[,2],'%Y')) <= time_periods[i,2]
+      
+      tps[TF_tp] <- i
+      
+    }  
+    
+    return(tps)
+    
+  } else {
+    
+    for(i in 1:nrow(time_periods)){
+      
+      TF_tp <- as.numeric(format(Date,'%Y')) >= time_periods[i,1] &
+        as.numeric(format(Date,'%Y')) <= time_periods[i,2]
+      
+      tps[TF_tp] <- i
+      
+    }
+    
+    return(tps)
+    
+  }
+  
+}
+
+# Internal function that uses the implementation of Telfer written by Gary Powney
+
+telfer_func <- function (taxa_data, iterations = 10, useIterations = TRUE, minSite = 5){
+  
+  # Check we have two time periods
+  TPs <- sort(unique(taxa_data$time_period))
+  if(length(unique(taxa_data$time_period)) != 2) stop('There are not two time periods in the data')
+  
+  # subset data to one object for each time perod
+  T1 <- unique(taxa_data[taxa_data$time_period == TPs[1], c('taxa', 'site')])
+  T2 <- unique(taxa_data[taxa_data$time_period == TPs[2], c('taxa', 'site')])
+  
+  # remove gridcells that are only found in one time period
+  T1cells <- unique(T1$site)
+  T2cells <- unique(T2$site)
+  allGood <- T1cells[T1cells %in% T2cells]
+  T1 <- T1[T1$site %in% allGood,]
+  T2 <- T2[T2$site %in% allGood,]
+  
+  ### Identify the number of sites each taxa occupies in each time period. ###
+  T1_range <- data.frame(taxa = names(table(T1$taxa)), T1Nsites = as.numeric(table(T1$taxa)))
+  T2_range <- data.frame(taxa = names(table(T2$taxa)), T2Nsites = as.numeric(table(T2$taxa)))
+  
+  ### Remove taxa which have less than minSite grid cells in first period ###
+  T1_range_good <- T1_range[T1_range$T1Nsites >= minSite,]
+  if(nrow(T1_range_good) == 0) stop(paste('No taxa satisfy the minSite criteria when comparing time',
+                                          'period', TPs[1], 'and', TPs[2]))
+  
+  ### Link the two tables by taxa ###
+  spp_table <- merge(T1_range_good, T2_range, by = 'taxa')
+  
+  ### Identify the simple difference between the two time periods ###
+  spp_table$range_change <- spp_table$T2Nsites - spp_table$T1Nsites 
+  
+  ### convert the grid cell number to proportion of total number of cells surveyed ###
+  total.cells <- length(allGood)
+  
+  # To avoid the problems associated with 0 proportions they were calculated as (x + 0.5) / (n + 1).
+  spp_table$T1_range_prop <- (spp_table$T1Nsites + 0.5) / (total.cells + 1)	
+  spp_table$T2_range_prop <- (spp_table$T2Nsites + 0.5) / (total.cells + 1)
+  
+  
+  spp_table$T1_logit_range <- log(spp_table$T1_range_prop / (1 - spp_table$T1_range_prop))  # logit transform the proportions
+  spp_table$T2_logit_range <- log(spp_table$T2_range_prop / (1 - spp_table$T2_range_prop))	# logit transform the proportions
+  
+  ### To account for non constant variance we must do the following steps to create a variable to weight the final regression ###
+  row.names(spp_table) <- spp_table$taxa # name rows helps make sense of the model output
+  m1 <- lm(T2_logit_range ~ T1_logit_range, data = spp_table)  	# linear regression of two time periods
+  
+  if(!useIterations){
+    
+    spp_table$change_index <- rstandard(m1)
+    final_output_table <- spp_table[,c(1:3,9)]
+    return(list(final_output_table, spp_table))
+    
+  } else if(useIterations){
+    
+    spp_table$sq_residual_m1 <- resid(m1)^2 # square the residuals
+    spp_table$fitted_proportions <- ilt(m1$fitted) # exponential function, overwrite fitted values to fitted proportions
+    
+    total.cell.1 <- total.cells + 1								# total grid cells surveyed + 1 
+    
+    spp_table$NP_test <- 1 / (total.cell.1 * spp_table$fitted_proportions * 
+                                (1 - spp_table$fitted_proportions))	# 1 / [NP (1 - P)] in telfer paper
+    
+    # second model which is the squared residuals of m1 on 1/[NP (1-P)]
+    m2 <- lm(sq_residual_m1 ~ NP_test, data = spp_table)
+    
+    # second model which is the squared residuals of m1 on 1/[NP (1-P)]
+    c_ <- coef(m2)[1] # take the intercept of m2 
+    d_ <- coef(m2)[2] # take the slope of m2
+    
+    V_ <- NULL												# prepare variance vector
+    rep_loop <- 1:(iterations-1)		# repeat the process 
+    C_all <- c_												# prepare c vector
+    D_all <- d_												# prepare d vector
+    V_all <- V_												# prepare variance vector
+    
+    
+    for (i in rep_loop){  							
+      
+      # c_ + d_ should not be in brackets as they were in a previous version
+      V_ <- c_ + d_ / (total.cell.1 * spp_table$fitted_proportions * (1 - spp_table$fitted_proportions))	# work out the variance (modification of the binomial proportion variance structure)
+      
+      # take the inverse of variance squared.
+      inv_sq_V <- 1 / (V_^2)										
+      
+      # run the new model weighting by the inverse variance identifeid above.
+      error <- try(m4 <- lm(sq_residual_m1 ~ NP_test, weights = inv_sq_V, data = spp_table), silent = TRUE)
+      #if(class(error) == 'try-error') stop('Model failed in iteration, too little data?')
+      
+      c_ <- coef(m4)[1] # take the intercept of the new model
+      d_ <- coef(m4)[2] # take the slope of the new model
+      
+      C_all <- c(C_all,c_) # build a vector with all of the new intercepts
+      D_all <- c(D_all,d_) # build a vector with all of the new slopes
+      V_all <- c(V_all,V_) # build a vector with all of the new inverse variances squared
+      
+    }
+    
+    ## final model to take the residuals from
+    # take the reciprocal of the "settled" variance (Telfer et al 2002)
+    spp_table$recip_V <- 1 / V_  										
+    # use the reciprocal of the "settled" variance as weight for the final model.
+    m1 <- lm(T2_logit_range ~ T1_logit_range, weights = spp_table$recip_V, data = spp_table)
+    # take the standardised residuals from the model.
+    # For each taxa, the standardised residual from the fitted regression line provides
+    # the index of relative change in range size. A taxa with a negative change index has
+    # been recorded in relatively fewer grid cells in the later period, whereas a taxa with
+    # a positive change index has been recorded in relatively more.  
+    spp_table$change_index <- rstandard(m1)								
+    final_output_table <- spp_table[,c(1:3,13)]	
+    
+    return(list(final_output_table, spp_table))
+    
+  }
+  
+}
+
+ilt <-
+  function(x) exp(x)/(1+exp(x))
 
 setwd("C:/BRERC")
-library(sparta)
-MyData<-read.csv("BRERC2.csv") #This is the CSV made in run first?
+#library(sparta)
+MyData<-read.csv("BRERC.csv") #This is the CSV made in run first?
 
 
 #Needs to be Y-M-D
@@ -470,7 +673,7 @@ class(MyData$dateofrecord)
 
 #Subset to get rid of pre 1960 as numbers too low for comparison with Telfer. 
 #you might not need to do this step with smaller datasets
-TelData <- subset(MyData, MyData$Year > 1959)
+#TelData <- subset(MyData, MyData$Year > 1959)
 
 #Telfer needs time periods, not dates
 ## Create a new column for the time period
@@ -479,8 +682,8 @@ TelData <- subset(MyData, MyData$Year > 1959)
 summary(TelData$dateofrecord)
 
 # No need to have this many time periods, can just be 2
-time_periods <- data.frame(start = c(1960, 1970, 1980, 1990, 2000, 2010, 2020),
-                           end = c(1969, 1979, 1989, 1999, 2009, 2019, 2029))
+time_periods <- data.frame(start = c(1950, 1960, 1970, 1980, 1990, 2000, 2010,2020),
+                           end = c(1959, 1969, 1979, 1989, 1999, 2009, 2019, 2029))
 
 
 # Now use these to assign my dates to time periods
