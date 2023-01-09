5573f6ac33311dc9f3cbd66e77d394993c56d359
Stavrin <steffanleprince@gmail.com>
2023-01-09 21:34:09 +0000

    Made an eastings/northings to x/y latlong converter. Mk3 OccAssess Rarity uses this.

---
+++ Don't Work Properly With Dummy Dataset Provided/OccAssess Rarity Bias mk3.R
@@ -0,0 +1,50 @@
+######Prep Dataset#####
+setwd("C:/BRERC")
+
+#library(lubridate)
+#library(tidyverse)
+#library(dplyr)
+library(occAssess)
+#library(rnrfa)
+#library(sparta)
+
+#MyData<-read.csv("ALLBCCv1pt1.csv") #This is the CSV you made in 'run first'
+MyData<-read.csv("BRERC.csv")
+
+
+
+str(MyData)
+
+periods <- list(1950:1959, 1960:1969, 1970:1979, 1980:1989, 1990:1999, 2000:2009, 2010:2019)
+
+#clean up data
+#MyData2 <- MyData[, c("Species", "east", "north", "Year", "Uncertainty",
+#                      "taxagroup")]
+
+#rm(MyData)
+
+#OccAssess requires NA for empty fields
+#rm(MyData2[MyData2 == '']) <- NA
+#print(MyData2)
+
+taxBias <- assessRarityBias(dat = MyData,
+                            periods = periods,
+                            res = 0.5,
+                            prevPerPeriod = FALSE,
+                            species = "Species",
+                            x = "x",
+                            y = "y",
+                            year = "Year", 
+                            spatialUncertainty = "Uncertainty",
+                            identifier = "taxagroup")
+
+
+
+
+str(taxBias$data)
+
+taxBias$plot +ggplot2::ylim(c(0,1))
+
+taxBias[["plot"]]
+
+#Sys.setenv('R_MAX_VSIZE'=32000000000)
